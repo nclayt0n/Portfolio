fc7557e8d344b240aad325ca2f95ed64220eb7d7
Nicole Clayton <50276082+nclayt0n@users.noreply.github.com>
2019-12-10 01:39:07 -0500

    took out anatomy quiz, added in recipebox and user-directory, deleted unused images, resized pictures, margins, and font-size. Also added to js- now when hover over the image it will show the repo and live links

--- main.js
+++ main.js
@@ -4,40 +4,53 @@ let currentHour = date.getHours();
 
 function greetingDisplay() {
     if (currentHour >= 5 && currentHour < 10) {
-        greeting = 'Good Morning!'
+        greeting = 'Good Morning,<br/> I\'m Nicole';
     }
     if (currentHour >= 10 && currentHour < 18) {
-        greeting = 'Good Afternoon!'
+        greeting = 'Good Afternoon,<br/> I\'m Nicole';
     }
     if (currentHour >= 18 && currentHour < 22) {
-        greeting = 'Good Evening!'
-    } else {
-        greeting = 'Good Night!'
+        greeting = 'Good Evening,<br/> I\'m Nicole';
+    }
+    if (currentHour >= 22 || currentHour < 5) {
+        greeting = 'Good Night,<br/> I\'m Nicole';
     }
 
-    $('.welcome').text(greeting);
-    $('.greeting').hover(event => {
-        $('.greeting').toggleClass('hidden');
-        $('.welcome').toggleClass('hidden');
-    })
-    $('.welcome').hover(event => {
-        $('.greeting').toggleClass('hidden');
-        $('.welcome').toggleClass('hidden');
-    })
+    $('h2.greeting').html(greeting);
 }
 
 function projectButtonToggle() {
 
     $('#anatomyButton').click(event => {
         $('.anatomyQuizDescription').toggleClass('hidden');
-    })
+    });
     $('#IPAButton').click(event => {
         $('.IPADescription').toggleClass('hidden');
-    })
+    });
+    $('#userDirectoryButton').click(event => {
+        $('.userDirectoryDescription').toggleClass('hidden');
+    });
+    $('#recipeBoxButton').click(event => {
+        $('.recipeBoxDescription').toggleClass('hidden');
+    });
+}
+
+function projectImageHover() {
+    $('.liveImgContainer').mouseenter(event => {
+        console.log(event.target.children);
+        $(event.target.children).removeClass('hidden');
+    });
+    $('.liveImgContainer').mouseleave(event => {
+        console.log(event.target.children);
+        $('.projectLinksContainer').addClass('hidden');
+    });
+
+
 }
 
 function ready() {
     projectButtonToggle();
     greetingDisplay();
+    projectImageHover();
 }
 $(ready);
