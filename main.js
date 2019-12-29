745858155b1f4d93c268aecdf1dd5c1ba12c86c9
Nicole Clayton <50276082+nclayt0n@users.noreply.github.com>
2019-12-29 15:41:40 -0500

    updated tablet view

--- main.js
+++ main.js
@@ -46,7 +46,103 @@ function projectImageHover() {
 
 }
 
+function getWindowWidth() {
+    console.log($('#recipeContainer').outerHeight(), $('#recipeContainer').height())
+    if ($(window).width() >= 600) {
+        let bioHeight = $('#bioContainer').innerHeight() + 215;
+        $('#recipeContainer').css({
+            'position': 'absolute',
+            'left': '220px',
+            'top': `${bioHeight}px`,
+            'min-width': '275px'
+        });
+        $('#userDirectoryContainer').css({
+            'position': 'absolute',
+            'left': '0px',
+            'top': `${$('#recipeContainer').innerHeight() + bioHeight + 30}px`,
+            'width': '50%'
+        });
+        $('#IPAContainer').css({
+            'position': 'absolute',
+            'right': '0px',
+            'top': `${$('#userDirectoryContainer').innerHeight() + bioHeight + $('#recipeContainer').innerHeight() + 60}px`,
+            'width': '50%'
+        });
+    }
+    if ($(window).width() < 600) {
+        $('#recipeContainer').css({
+            'position': 'static',
+            'margin': '30px 40px',
+            'padding': '25px'
+        });
+        $('#userDirectoryContainer').css({
+            'position': 'static',
+            'margin': '30px 40px',
+            'padding': '25px',
+        });
+        $('#IPAContainer').css({
+            'position': 'static',
+            'margin': '30px 40px',
+            'padding': '25px',
+        });
+    }
+    $(window).resize(function() {
+        let bioResizedHeight = $('#bioContainer').innerHeight() + 175;
+        let recipeHeight = $('#recipeContainer').innerHeight() + 30;
+        let userDirectoryHeight = $('#userDirectoryContainer').innerHeight() + 30;
+        let IPAHeight = $('#IPAContainer').innerHeight() + 30;
+
+        if ($(window).width() >= 600) {
+            $('#recipeContainer').css({
+                'position': 'absolute',
+                'left': '220px',
+                'top': `${bioResizedHeight}px`,
+                'min-width': '275px'
+            });
+            $('#userDirectoryContainer').css({
+                'position': 'absolute',
+                'left': '0px',
+                'top': `${recipeHeight+bioResizedHeight}px`,
+                'width': '50%'
+            });
+            $('#IPAContainer').css({
+                'position': 'absolute',
+                'right': '0px',
+                'top': `${recipeHeight+bioResizedHeight+ userDirectoryHeight}px`,
+                'width': '50%'
+            });
+            // $('#contactContainer').css({
+            //     'position': 'absolute',
+            //     'bottom': `${recipeHeight+bioResizedHeight+ userDirectoryHeight}px`,
+            // });
+        }
+
+        if ($(window).width() < 600) {
+            $('#recipeContainer').css({
+                'position': 'static',
+                'margin': '30px 40px',
+                'padding': '25px'
+            });
+            $('#userDirectoryContainer').css({
+                'position': 'static',
+                'margin': '30px 40px',
+                'padding': '25px',
+                'width': '75%'
+            });
+            $('#IPAContainer').css({
+                'position': 'static',
+                'margin': '30px 40px',
+                'padding': '25px',
+                'width': '75%'
+            });
+
+        }
+
+    });
+}
+
 function ready() {
+    getWindowWidth();
     projectButtonToggle();
     greetingDisplay();
     projectImageHover();
